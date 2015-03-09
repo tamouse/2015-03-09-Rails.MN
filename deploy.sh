9fe03e2b999b501354b2edc2387448f21f73434a
Tamara Temple <tamouse@gmail.com>
2015-03-09 14:37:57 -0500

    more clean up

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,9 @@
 set -x
 : ${DEPLOY_DIR:=_gh-pages}
 : ${DEPLOY_BRANCH:=gh-pages}
-GIT=/usr/local/bin/git
-/usr/local/bin/pandoc -o $DEPLOY_DIR/index.html README.md
-/bin/cp 2015-03-09-rails.mn-presentation.pdf $DEPLOY_DIR
-(cd $DEPLOY_DIR && $GIT checkout $DEPLOY_BRANCH && $GIT add --all --verbose && $GIT commit -m "$(date)" && $GIT push -fu origin HEAD)
+/usr/local/bin/convert -resize 16x16 railsmn_logo.png $DEPLOY_DIR/favicon.ico && \
+/usr/local/bin/pandoc -s -o $DEPLOY_DIR/index.html -H favicon-include.html README.md && \
+/bin/cp 2015-03-09-rails.mn-presentation.pdf $DEPLOY_DIR && \
+GIT=/usr/local/bin/git && \
+(cd $DEPLOY_DIR && $GIT checkout $DEPLOY_BRANCH && $GIT add --all --verbose && $GIT commit -m "$(date)" && $GIT push -fu origin HEAD) && \
 echo "Deployed"
